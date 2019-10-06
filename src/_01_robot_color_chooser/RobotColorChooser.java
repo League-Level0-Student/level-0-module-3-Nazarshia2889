2c9278fa1bade2fb8ed09915b688b45ad2383a11
League Student <league@iMac-9.attlocal.net>
2019-10-06 15:56:53 -0700

    StartingModule3

--- src/_01_robot_color_chooser/RobotColorChooser.java
+++ src/_01_robot_color_chooser/RobotColorChooser.java
@@ -4,24 +4,53 @@
 package _01_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
+import java.util.Random;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		//1. Create a new Robot
+		Robot walle = new Robot();
 		
+		for(int i = 0;i<100;i++) {
 		//3. Ask the user what color they would like the robot to draw
+		String color = JOptionPane.showInputDialog(null, "What color would you like the robot to draw?");
 		
 		//5. Use an if/else statement to set the pen color that the user requested
+		if(color.equalsIgnoreCase("blue")){
+			walle.setPenColor(Color.BLUE);
+		}
+		if(color.equalsIgnoreCase("green")){
+			walle.setPenColor(Color.GREEN);
+		}
+		if(color.equalsIgnoreCase("red")){
+			walle.setPenColor(Color.RED);
+		}
+		if(color.equalsIgnoreCase("yellow")){
+			walle.setPenColor(Color.YELLOW);
+		}
 
         //6. If the user doesn’t enter anything, choose a random color
+		else {
+			walle.setRandomPenColor();
+		}
 
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 		//4. Set the pen width to 10
+		walle.setPenWidth(10);
 		
 	    //2. Make the robot draw a shape (this will take more than one line of code)
-
+		for(int x = 0;x<4;x++) {
+			walle.penDown();
+			walle.setSpeed(100);
+			walle.move(100);
+			walle.turn(90);
+		}
+		}
 
 	}
 }
--- src/_03_high_low/HighLowGame.java
+++ src/_03_high_low/HighLowGame.java
@@ -5,20 +5,33 @@ package _03_high_low;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
+		int random = new Random().nextInt((100-1)-1)+1;
 		
 		// 2. Print out the random variable above
+		System.out.println(random);
 		
 		// 11. Repeat steps 1 to 10 ten times
 		
 			// 1. Ask the user for a guess using a pop-up window, and save their response 
+		String guess = JOptionPane.showInputDialog(null, "Guess the number:");
 
 			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
-			
+			int x = Integer.parseInt(guess);
+			if(x==random) {
+				JOptionPane.showMessageDialog(null, "You won!");
+			}
+			else if(x>random) {
+				JOptionPane.showMessageDialog(null, "Your number is too high!");
+			}
+			else if(x<random) {
+				JOptionPane.showMessageDialog(null, "Your number is too low!");
+			}
 			// 5. if the guess is correct
 				// 6. Win
 				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
